754b5bd1b20acbe0156f8731804f920941eae9dc
Fernando Agüero <fjaguero@gmail.com>
2017-07-20 13:46:39 +0200

    Use prop-types packages instead of React.PropTypes

--- src/TagsInput.jsx
+++ src/TagsInput.jsx
@@ -1,5 +1,6 @@
 import Clipboard from 'clipboard';
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import mousetrap from 'mousetrap';
 import {
